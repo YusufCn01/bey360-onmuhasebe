17ca90440119b3530b035f8185f3b6f844e61ee1
RAFIK <yusufcanbrk@gmail.com>
2026-06-08 18:15:30 +0300

    fix(build): remove Turbopack workaround and use webpack for build to fix standalone middleware

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.0.18", "isa-above-equally-cio.trycloudflare.com"],
-  // Turbopack'in manuel oluşturduğumuz .nft.json dosyasını silmemesi için:
-  cleanDistDir: false,
 };
 
 export default nextConfig;
