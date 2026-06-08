393a6e0a5ca1eda5ad8f45f1d0c092a149334d80
RAFIK <yusufcanbrk@gmail.com>
2026-06-08 17:15:33 +0300

    Force Webpack to bypass Turbopack middleware.js.nft.json build error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.0.18", "isa-above-equally-cio.trycloudflare.com"],
+  // Turbopack'in middleware tracing hatasını aşmak için Webpack derleyicisine dönmeye zorluyoruz:
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
