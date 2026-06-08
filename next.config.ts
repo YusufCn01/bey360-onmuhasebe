6777c1ac903be8cb579e039f28ef7d21579735b3
RAFIK <yusufcanbrk@gmail.com>
2026-06-08 16:41:06 +0300

    Move middleware to src directory and revert config to fix nft.json build error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.0.18", "isa-above-equally-cio.trycloudflare.com"],
-  outputFileTracing: false,
 };
 
 export default nextConfig;
