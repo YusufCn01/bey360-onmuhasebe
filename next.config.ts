21224e2965d145622237122f0603f8c9467b88bc
RAFIK <yusufcanbrk@gmail.com>
2026-06-08 17:23:26 +0300

    Inject missing middleware.js.nft.json before build to bypass Turbopack bug

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.0.18", "isa-above-equally-cio.trycloudflare.com"],
-  // Turbopack'in middleware tracing hatasını aşmak için Webpack derleyicisine dönmeye zorluyoruz:
-  webpack: (config) => {
-    return config;
-  },
+  // Turbopack'in manuel oluşturduğumuz .nft.json dosyasını silmemesi için:
+  cleanDistDir: false,
 };
 
 export default nextConfig;
